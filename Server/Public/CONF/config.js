31b384943cf1c98de805b0767dfd5ff7d8f63061
Miguel Moreno <mmd.-@hotmail.com>
2025-04-08 19:28:46 -0600

    .

--- Server/Public/CONF/config.js
+++ Server/Public/CONF/config.js
@@ -4,6 +4,6 @@ module.exports = {
     USER: "root",
     PASSWORD: "",
     DATABASE_NAME: "POI",
-    SERVER_URL: "http://192.168.100.9:2800"
-    // SERVER_URL: "http://192.168.100.17:2800"
+    SERVER_URL: "http://192.168.100.9:2800" //PC
+    // SERVER_URL: "http://192.168.100.17:2800" //LAPTOP
 };
